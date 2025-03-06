bd9c5e8972bf561df8b3f53c9ec0c4253191e90c
Arnob <arnobdeb10@gmail.com>
2025-03-06 11:19:46 +0600

    Initial commit - Added Next.js project

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import HeroSection from "@/components/hero";
 
 export default function Home() {
   return (
     <div>
-      Welcome to My Sensei
-      <br />
-      <Button>Hello</Button> 
-      </div>
+      <div className="grid-background"></div>
+
+      <HeroSection />
+    </div>
   );
 }
