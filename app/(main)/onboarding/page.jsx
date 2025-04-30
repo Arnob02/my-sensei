ab982cd44589130646104558714288211de5418a
Arnob <arnobdeb10@gmail.com>
2025-04-30 14:13:55 +0600

    User.JS

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,19 +1,19 @@
-import { getUserOnboardingStatus } from "@/actions/user";
+import { redirect } from "next/navigation";
 import { industries } from "@/data/industries";
-import React from "react";
+import OnboardingForm from "./_components/onboarding-form";
+import { getUserOnboardingStatus } from "@/actions/user";
 
-const OnBoardingPage = async () => {
+export default async function OnboardingPage() {
   // Check if user is already onboarded
   const { isOnboarded } = await getUserOnboardingStatus();
+
   if (isOnboarded) {
-    // Redirect to dashboard or home page
     redirect("/dashboard");
   }
+
   return (
     <main>
-      <OnBoardingForm industries={industries} />
+      <OnboardingForm industries={industries} />
     </main>
   );
-};
-
-export default OnBoardingPage;
+}
--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider";
-import Header from "@/components/header";
+import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { dark, light } from "@clerk/themes";
+import { Toaster } from "sonner";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+import { dark } from "@clerk/themes";
 
 import "./globals.css";
 
@@ -32,6 +34,8 @@ export default function RootLayout({ children }) {
             {/* header */}
             <Header />
             <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+
             {/* footer */}
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
